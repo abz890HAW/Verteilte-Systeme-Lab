e2b86dcaca8d7716cd4c9a6f1208da8956696600
marble <marble@chaoscomputer.club>
2018-10-08 17:28:09 +0200

    workign local chat

--- lab1/src/CMessageClient.java
+++ lab1/src/CMessageClient.java
@@ -1,28 +1,129 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class CMessageClient {
+public class CMessageClient extends Application {
+
+    /* fxml related variables */
+    private Timeline timeline;
+    @FXML
+    private TextField textFieldInput;
+    @FXML
+    private TextField textFieldHost;
+    @FXML
+    private Button buttonSend;
+    @FXML
+    private Button buttonConnect;
+    @FXML
+    private ListView listViewOutput;
+
+    /* RMI relevant variables */
     private IMessageService messageService;
+    private static String clientID;
+    Timer timer = new Timer();
 
-    public CMessageClient() throws RemoteException, NotBoundException {
-        Registry registry = LocateRegistry.getRegistry();
-        messageService = (IMessageService) registry.lookup(IMessageService.REGISTRY_IDENTIFIER);
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
+    @Override
+    public void stop(){
+        System.out.println("Stage is closing");
+        System.exit(0);
+    }
 
     public static void main(String[] args) {
         try {
-            CMessageClient messageClient = new CMessageClient();
-        }
-        catch (NotBoundException e) {
-            System.err.println("Could not find remote object in Registry.");
+            clientID = Inet4Address.getLocalHost().getHostAddress();
+            launch(args);
+        } catch (UnknownHostException e) {
+            System.err.println("Could not get IP address of localhost.");
             e.printStackTrace();
+        }
+    }
+
+    public void send(ActionEvent e) {
+        String text = textFieldInput.getText();
+        try {
+            messageService.newMessage(clientID, text);
+        }
+        catch (RemoteException ex) {
+            System.err.println("Communication to RMI server disrupted.");
+            ex.printStackTrace();
+        }
+        textFieldInput.setText("");
+    }
+
+    /* try to establish connection to RMI server and get remote object
+     * print to output text field on success
+     */
+    public void connect(ActionEvent e) {
+        String host = textFieldHost.getText();
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            messageService = (IMessageService) registry.lookup(IMessageService.REGISTRY_IDENTIFIER);
+            buttonConnect.setDisable(true);
+            textFieldHost.setDisable(true);
+            buttonSend.setDisable(false);
+            timeline = new Timeline(new KeyFrame(Duration.millis(100), ae -> pollMessages()));
+            timeline.setCycleCount(Animation.INDEFINITE);
+            timeline.play();
+        }
+        catch (RemoteException ex) {
+            System.err.println("Could not establish communication to RMI server.");
+            ex.printStackTrace();
+        }
+        catch (NotBoundException ex) {
+            System.err.println("Could not find remote object in Registry.");
+            ex.printStackTrace();
+        }
+        listViewOutputWrite("Connection established.");
+    }
+
+    private void listViewOutputWrite(String string) {
+        ObservableList items = listViewOutput.getItems();
+        items.add(string);
+        listViewOutput.scrollTo(items.size()-1);
+    }
+
+    public void pollMessages()
+    {
+        try {
+            String message = messageService.nextMessage(clientID);
+            if(null != message) {
+                listViewOutputWrite(message);
+            }
         }
         catch (RemoteException e) {
-            System.err.println("Could not establish communication to RMI server.");
             e.printStackTrace();
         }
+
     }
 }
--- lab1/src/CMessageService.java
+++ lab1/src/CMessageService.java
@@ -2,21 +2,46 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class CMessageService extends UnicastRemoteObject implements IMessageService {
+    // struct to hold data regarding one client
+    private class Client
+    {
+        public Queue<String> messageQueue = new LinkedList<String>();
+        public long lastAccess;
+    };
 
+    // dictionary mapping clientID to client object
+    private static Map<String, Client> clientDictionary = new HashMap<String, Client>();
+    private static final int TIMEOUT_MS = 1000;
+    private int messageID = 0;
+
+    // constructor
     public CMessageService() throws RemoteException {
         super();
     }
 
     @Override
     public String nextMessage(String clientID) throws RemoteException {
-        return null;
+        // if client ID isn't registeres, create new entry for it
+        if(clientDictionary.get(clientID) == null) {
+            clientDictionary.put(clientID, new Client());
+            System.out.println("Adding message queue for " + clientID);
+        }
+        Client client = clientDictionary.get(clientID);
+        client.lastAccess = System.currentTimeMillis();
+        return clientDictionary.get(clientID).messageQueue.poll();
     }
 
     @Override
     public void newMessage(String clientID, String message) throws RemoteException {
-
+        // enqueue message to all clients
+        String text = String.format("%06d %16s : %-80s %08d", messageID++, clientID, message, System.currentTimeMillis());
+        for(String key : clientDictionary.keySet()) {
+            clientDictionary.get(key).messageQueue.add(text);
+        }
     }
 
     public static void main(String[] args) {
@@ -25,9 +50,22 @@ public class CMessageService extends UnicastRemoteObject implements IMessageServ
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind(IMessageService.REGISTRY_IDENTIFIER, messageService);
             System.out.println("Server initialized.");
+            /* timeout loop */
+            while(true) {
+                // TODO implement timeout logic here
+                TimeUnit.MILLISECONDS.sleep(TIMEOUT_MS);
+                for(String key : clientDictionary.keySet()) {
+                    if(System.currentTimeMillis()-clientDictionary.get(key).lastAccess > TIMEOUT_MS) {
+                        clientDictionary.remove(key);
+                        System.out.println("Dropping message queue for " + key);
+                    }
+                }
+            }
         }
         catch(RemoteException e) {
             System.err.println("Error during initialization of date server");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
